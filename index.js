0e08639a4388e3ac244cf5ff8c81911227dd381c
vccd储诚栋 <cdchu@ctrip.com>
2018-03-26 11:15:55 +0800

    fix mac & windows bugs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var crypto = require('crypto');
+
 process.dlopen(module, require.resolve('./build/Release/binding.node'));
 
 exports.SIZE_DEFAULT = 6;
@@ -10,6 +12,28 @@ exports.SIZE_2K = 11;
 exports.SIZE_4K = 12;
 exports.SIZE_8K = 13;
 exports.SIZE_16K = 14;
+
+var NativeCache = exports.Cache;
+
+var cacheNameMap = {};
+
+function md5 () {
+	var md5sum = crypto.createHash('md5');
+	md5sum.update(str);
+	return md5sum.digest('hex');
+}
+
+function Cache (name, size, blockSize) {
+	var md5Name;
+	if (cacheNameMap.hasOwnProperty(name)) {
+		md5Name = cacheNameMap[name];
+	} else {
+		md5Name = md5(name).slice(8, 24);
+	}
+	return new NativeCache(md5Name, size, blockSize);
+}
+
+exports.Cache = Cache;
 
 if(process.mainModule === module && process.argv[2] === 'release') {
 	process.argv.slice(3).forEach(exports.release);
